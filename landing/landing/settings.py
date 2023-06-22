713c323fb0eb42de8093c3a65d398029d62e9eb1
Nikeshhh <WityazKajit@gmail.com>
2023-06-22 17:14:37 +0300

    Подключен Postgres, создано приложение

--- landing/landing/settings.py
+++ landing/landing/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'landing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PracticeProject',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',  # TODO: пароль инкапсулировать в окружение
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ landing/landing_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LandingBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'landing_backend'
