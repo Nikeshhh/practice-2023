8b7055298a21025117c35f2f47da577db5c5013f
Nikeshhh <WityazKajit@gmail.com>
2023-06-26 19:02:29 +0300

    Добавлены модели для контактных данных, карточек наборов услуг, сделана админка для этих моделей

--- landing/landing_backend/admin.py
+++ landing/landing_backend/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import TestModel
+from .models import TestModel, CompanyContacts, Services, ServiceTypes
 
-admin.site.register(TestModel)
+admin.site.register(Services)
+admin.site.register(ServiceTypes)
+
+
+class CompanyContactsAdmin(admin.ModelAdmin):
+    fields = [('company_name', 'inn'), ('skype', 'whatsapp', 'telegram'), ('phone_number', 'address', 'email')]
+
+
+admin.site.register(CompanyContacts, CompanyContactsAdmin)
--- landing/landing_backend/models.py
+++ landing/landing_backend/models.py
@@ -6,3 +6,36 @@ class TestModel(models.Model):
 
     def __str__(self):
         return f'{self.test_field}'
+
+
+class CompanyContacts(models.Model):
+    company_name = models.CharField(max_length=80, verbose_name='Наименование компании')
+    inn = models.CharField(max_length=15, verbose_name='ИНН')
+    skype = models.CharField(max_length=32, verbose_name='Логин Skype')
+    whatsapp = models.CharField(max_length=64, verbose_name='Номер Whatsapp')
+    telegram = models.CharField(max_length=32, verbose_name='Логин Telegram')
+    phone_number = models.CharField(max_length=12, verbose_name='Контактный номер')
+    address = models.CharField(max_length=100, verbose_name='Адрес компании')
+    email = models.CharField(max_length=32, verbose_name='Электронная почта')
+
+    def __str__(self):
+        return self.company_name
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class ServiceTypes(models.Model):
+    name = models.CharField(max_length=25)
+    price = models.IntegerField()
+    related_services = models.ManyToManyField(Services)
+
+    def __str__(self):
+        return self.name
+
+
+
