dbf46fc8cc8af4d21535c2024712fae961702789
Nikeshhh <WityazKajit@gmail.com>
2023-06-29 15:50:46 +0300

    Настроен доступ к дев серверу с телефона

--- landing/landing/settings.py
+++ landing/landing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f=+exek5jky^qdu&v_!%nswn7$oard+u-3j%_7-t%i&jogt=r-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.3.14', 'localhost']
 
 # Application definition
 
